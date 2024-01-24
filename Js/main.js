e8979a3d42d25027124cf685b24e9762a2c57b37
Ana Sena <luiza1fofa@gmail.com>
2024-01-23 22:51:58 -0300

    Revert "comitando formulário com máscaras"
    This reverts commit 5780890eb2f6bc4e2de6b896ad6bafd53e2ebb66.

--- Js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-
-    $('#cpf').mask('000.000.000-00', {placeholder: '___.___.___-__'})
-    $('#telefone').mask('(00) 00000-0000', {placeholder: '(__) _____-_____'})
-    $('#cep').mask('00000-000', {placeholder: '_____-__'})
-})

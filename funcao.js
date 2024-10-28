821102258b08ca52079c21540907fd8e9bc2c6b0
Ana Sena <luiza1fofa@gmail.com>
2024-10-28 11:34:53 -0300

    criando a funcao para o exercicio

---
+++ funcao.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicar(5, 5));
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+console.log(dizOla('Kaio Colares'));

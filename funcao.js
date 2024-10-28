91d3caa142c4e583d26bdb5e0bc8d4054e7203a0
Ana Sena <luiza1fofa@gmail.com>
2024-10-28 11:36:39 -0300

    criando a funcao para o exercicio com modificacao

--- funcao.js
+++ funcao.js
@@ -2,8 +2,6 @@
 function multiplicar(num1, num2) {
     return num1 * num2;
 }
-console.log(multiplicar(5, 5));
 function dizOla(nome) {
     return `Olá ${nome}`;
 }
-console.log(dizOla('Kaio Colares'));
--- funcao.ts
+++ funcao.ts
@@ -2,10 +2,9 @@ function multiplicar(num1: number, num2: number): number{
     return num1 * num2
 }
 
-console.log(multiplicar(5,5));
+
 
 function dizOla(nome:string):string{
     return `Olá ${nome}`;
 }
 
-console.log(dizOla('Kaio Colares'));
